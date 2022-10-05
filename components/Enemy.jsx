498303992eb2e69ce3c67e8cabfa54e180693e11
maalrocks <MaalRocks@users.noreply.github.com>
2022-10-05 09:59:42 +0200

    k

--- components/Enemy.jsx
+++ components/Enemy.jsx
@@ -1,27 +1,27 @@
 const Enemy = ({ enemy, onClick, index, className }) => {
-	return (
-		<div
-			onClick={onClick}
-			id={index}
-			className={className}
-		>
-			<div className=" p-2">
-				<div id={index}>
-					{enemy.name} {enemy.isDead ? "dead" : ""}
-				</div>
-				<div id={index}> index im state: {index} </div>
-				<div id={index}>
-					LP: {enemy.lp} / {enemy.maxLp}
-				</div>
-				<div id={index}>
-					MP: {enemy.mp} / {enemy.maxMp}
-				</div>
-				<div id={index}>
-					EXP: {enemy.exp} / {enemy.maxExp}
-				</div>
-			</div>
-		</div>
-	)
+  return (
+    <div
+      onClick={onClick}
+      id={index}
+      className={className}
+    >
+      <div className=" p-2">
+        <div id={index}>
+          {enemy.name} {enemy.isDead ? "dead" : ""}
+        </div>
+        <div id={index}> index im state: {index} </div>
+        <div id={index}>
+          LP: {enemy.lp} / {enemy.maxLp}
+        </div>
+        <div id={index}>
+          MP: {enemy.mp} / {enemy.maxMp}
+        </div>
+        <div id={index}>
+          EXP: {enemy.exp} / {enemy.maxExp}
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Enemy
--- pages/[id]/index.jsx
+++ pages/[id]/index.jsx
@@ -6,89 +6,89 @@ import Enemy from "../../components/Enemy"
 import Log from "../../components/Logs/Log"
 
 const Dashboard = () => {
-	const [player, setPlayer] = useState({})
-	const [enemies, setEnemies] = useState([])
-	const [target, setTarget] = useState()
-	const [battleLog, setBattleLog] = useState([])
-	const [log, setLog] = useState([])
-	const [message, setMessage] = useState()
-	const [turn, setTurn] = useState("player")
+  const [player, setPlayer] = useState({})
+  const [enemies, setEnemies] = useState([])
+  const [target, setTarget] = useState()
+  const [battleLog, setBattleLog] = useState([])
+  const [log, setLog] = useState([])
+  const [message, setMessage] = useState()
+  const [turn, setTurn] = useState("player")
 
-	// eventuell direkt eine Kategorie mitgeben z.B Kampf/Battle
-	// diese dann als Object speichern
-	// logEntry = {message:"der inhalt der Nachricht", category:"battle"}
-	// diesen logEntry irgendwo zwischenspeichern(DB/JSON-file?)
-	// nur die message per setLog übergeben
-	// soll bei späterer sortierung der log nachrichten helfen (logs: general/event/battle/conversation)
-	const writeToLog = (message, category) => {
-		setLog((prevLog) => prevLog + `${message}\n`)
-	}
+  // eventuell direkt eine Kategorie mitgeben z.B Kampf/Battle
+  // diese dann als Object speichern
+  // logEntry = {message:"der inhalt der Nachricht", category:"battle"}
+  // diesen logEntry irgendwo zwischenspeichern(DB/JSON-file?)
+  // nur die message per setLog übergeben
+  // soll bei späterer sortierung der log nachrichten helfen (logs: general/event/battle/conversation)
+  const writeToLog = (message, category) => {
+    setLog((prevLog) => prevLog + `${message}\n`)
+  }
 
-	// useEffect Hook holt beim ersten rendern infos aus der DB
-	// und schreibt diese in states
-	useEffect(() => {
-		const getPlayer = async () => {
-			try {
-				const res = await fetch(`/api/player/`)
+  // useEffect Hook holt beim ersten rendern infos aus der DB
+  // und schreibt diese in states
+  useEffect(() => {
+    const getPlayer = async () => {
+      try {
+        const res = await fetch(`/api/player/`)
 
-				if (!res.ok) {
-					throw new Error(res.status)
-				}
+        if (!res.ok) {
+          throw new Error(res.status)
+        }
 
-				const { data } = await res.json()
+        const { data } = await res.json()
 
-				setPlayer(data[0])
-			} catch (error) {
-				setMessage("Failed to get player")
-			}
-		}
+        setPlayer(data[0])
+      } catch (error) {
+        setMessage("Failed to get player")
+      }
+    }
 
-		const getEnemies = async () => {
-			try {
-				const res = await fetch(`/api/enemies/`)
+    const getEnemies = async () => {
+      try {
+        const res = await fetch(`/api/enemies/`)
 
-				if (!res.ok) {
-					throw new Error(res.status)
-				}
+        if (!res.ok) {
+          throw new Error(res.status)
+        }
 
-				const { data } = await res.json()
+        const { data } = await res.json()
 
-				setEnemies(data)
-			} catch (error) {
-				setMessage("Failed to get Enemy")
-			}
-		}
+        setEnemies(data)
+      } catch (error) {
+        setMessage("Failed to get Enemy")
+      }
+    }
 
-		getPlayer()
-		getEnemies()
-	}, [])
+    getPlayer()
+    getEnemies()
+  }, [])
 
-	const getRandomEnemyFromEnemiesState = () => {
-		const randomEnemyIndex = Math.floor(
-			Math.random() * (enemies.length - 0) + 0
-		)
+  const getRandomEnemyFromEnemiesState = () => {
+    const randomEnemyIndex = Math.floor(
+      Math.random() * (enemies.length - 0) + 0
+    )
 
-		console.log("asdjoafh: ", randomEnemyIndex)
-	}
+    console.log("asdjoafh: ", randomEnemyIndex)
+  }
 
-	const attack = (attacker, target) => {
-		// check for essential data
-		if (!attacker) return writeToLog("Kein Angreifer angegeben")
-		if (!target) return writeToLog("Kein Ziel ausgewählt")
-		if (target.isDead) return writeToLog("Das Ziel ist bereits besiegt")
+  const attack = (attacker, target) => {
+    // check for essential data
+    if (!attacker) return writeToLog("Kein Angreifer angegeben")
+    if (!target) return writeToLog("Kein Ziel ausgewählt")
+    if (target.isDead) return writeToLog("Das Ziel ist bereits besiegt")
 
-		// who attacks who - logMessage
-		writeToLog(`${attacker.name} greift ${target.name} an`)
+    // who attacks who - logMessage
+    writeToLog(`${attacker.name} greift ${target.name} an`)
 
-		// calculate damage
-		const damageAfterDefence = attacker.attack - target.defence
-		target.lp -= damageAfterDefence
+    // calculate damage
+    const damageAfterDefence = attacker.attack - target.defence
+    target.lp -= damageAfterDefence
 
-		// how much damage - logMessage
-		writeToLog(`${attacker.name} verursacht ${damageAfterDefence} Schaden`)
-	}
+    // how much damage - logMessage
+    writeToLog(`${attacker.name} verursacht ${damageAfterDefence} Schaden`)
+  }
 
-	/*  
+  /*  
 		BATTLE:
 		- 1 Check wessen turn es ist (init?)
 		- 2 wähle ziel
@@ -107,87 +107,87 @@ const Dashboard = () => {
 		- 5 Wenn enemy dran ist: führe angriff nach der reihenfolge aus die das enemiesArray vorgibt
 	*/
 
-	const handleClick = (event) => {
-		if (!event.target) return
+  const handleClick = (event) => {
+    if (!event.target) return
 
-		getRandomEnemyFromEnemiesState()
+    getRandomEnemyFromEnemiesState()
 
-		const currentTarget = event.target
-		const enemyIndex = currentTarget.id
+    const currentTarget = event.target
+    const enemyIndex = currentTarget.id
 
-		// get enemy from state by index
-		const enemy = enemies[enemyIndex]
+    // get enemy from state by index
+    const enemy = enemies[enemyIndex]
 
-		// write current index into targets index
-		enemy.index = Number.parseInt(enemyIndex)
+    // write current index into targets index
+    enemy.index = Number.parseInt(enemyIndex)
 
-		// set target to
-		setTarget(enemy)
+    // set target to
+    setTarget(enemy)
 
-		// write into generall log
-		writeToLog(`aktuelles Ziel: ${enemy.name}`)
-	}
+    // write into generall log
+    writeToLog(`aktuelles Ziel: ${enemy.name}`)
+  }
 
-	const checkIfTargetIsDead = (target) => {
-		if (target.lp <= 0) {
-			writeToLog(`${target.name} is dead`)
-			return true
-		} else return false
-	}
+  const checkIfTargetIsDead = (target) => {
+    if (target.lp <= 0) {
+      writeToLog(`${target.name} is dead`)
+      return true
+    } else return false
+  }
 
-	const handleAttack = () => {
-		// scope data to function
-		const currentTarget = target
-		const enemiesTmp = [...enemies]
+  const handleAttack = () => {
+    // scope data to function
+    const currentTarget = target
+    const enemiesTmp = [...enemies]
 
-		attack(player, currentTarget)
+    attack(player, currentTarget)
 
-		// check if attack has killed target
-		currentTarget.isDead = checkIfTargetIsDead(currentTarget)
+    // check if attack has killed target
+    currentTarget.isDead = checkIfTargetIsDead(currentTarget)
 
-		// write updated enemy into enemiesTmp
-		enemiesTmp[currentTarget.index] = currentTarget
+    // write updated enemy into enemiesTmp
+    enemiesTmp[currentTarget.index] = currentTarget
 
-		// update state with tempData
-		setEnemies(enemiesTmp)
+    // update state with tempData
+    setEnemies(enemiesTmp)
 
-		enemies.forEach((enemy) => {
-			if (!enemy.isDead) {
-				attack(enemy, player)
-				console.log(player)
-			}
-		})
-	}
+    enemies.forEach((enemy) => {
+      if (!enemy.isDead) {
+        attack(enemy, player)
+        console.log(player)
+      }
+    })
+  }
 
-	// Render HTML mit JSX Werten
-	return (
-		<>
-			{/* Header componente mit meta informationen für die Seite */}
-			<Head>
-				<title>Dashboard</title>
-			</Head>
+  // Render HTML mit JSX Werten
+  return (
+    <>
+      {/* Header componente mit meta informationen für die Seite */}
+      <Head>
+        <title>Dashboard</title>
+      </Head>
 
-			{/* Main Block - Hauptinhalt der Seite */}
-			<div className="flex h-screen w-full flex-col">
-				<div className="flex h-full flex-col flex-wrap items-center justify-evenly">
-					<div className="flex w-full justify-evenly">
-						{enemies.map((enemy, index) => (
-							<Enemy
-								key={index}
-								index={index}
-								enemy={enemy}
-								className={"bg-red-500 bg-opacity-50"}
-								onClick={handleClick}
-							/>
-						))}
-					</div>
+      {/* Main Block - Hauptinhalt der Seite */}
+      <div className="flex h-screen w-full flex-col">
+        <div className="flex h-full flex-col flex-wrap items-center justify-evenly">
+          <div className="flex w-full justify-evenly">
+            {enemies.map((enemy, index) => (
+              <Enemy
+                key={index}
+                index={index}
+                enemy={enemy}
+                className={"bg-red-500 bg-opacity-50"}
+                onClick={handleClick}
+              />
+            ))}
+          </div>
 
-					<div className="flex w-full justify-evenly">
-						<div className="bg-gray-500 bg-opacity-30 p-2">
-							<Player player={player} />
-							<div className="m-2">
-								<button
-									className="
+          <div className="flex w-full justify-evenly">
+            <div className="bg-gray-500 bg-opacity-30 p-2">
+              <Player player={player} />
+              <div className="m-2">
+                <button
+                  className="
 									m-1
 									bg-white
 									p-1
@@ -195,12 +195,12 @@ const Dashboard = () => {
 									hover:bg-gray-700
 									hover:text-gray-300
 								   "
-									onClick={handleAttack}
-								>
-									angriff
-								</button>
-								<button
-									className="
+                  onClick={handleAttack}
+                >
+                  angriff
+                </button>
+                <button
+                  className="
 									m-1
 									bg-white
 									p-1
@@ -208,11 +208,11 @@ const Dashboard = () => {
 									hover:bg-gray-700
 									hover:text-gray-300
 								   "
-								>
-									verteidigen
-								</button>
-								<button
-									className="
+                >
+                  verteidigen
+                </button>
+                <button
+                  className="
 									m-1
 									bg-white
 									p-1
@@ -220,18 +220,18 @@ const Dashboard = () => {
 									hover:bg-gray-700
 									hover:text-gray-300
 								   "
-								>
-									fliehen
-								</button>
-							</div>
-						</div>
+                >
+                  fliehen
+                </button>
+              </div>
+            </div>
 
-						<Log log={log} />
-					</div>
-				</div>
-			</div>
-		</>
-	)
+            <Log log={log} />
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 // Export Component
